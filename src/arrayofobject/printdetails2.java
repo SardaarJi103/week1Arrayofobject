2a93313c642738e0bb306b83b9e758e1a357ce8f
SardaarJi103 <59849600+SardaarJi103@users.noreply.github.com>
2020-01-26 13:11:47 -0500

    Update printdetails2.java

--- src/arrayofobject/printdetails2.java
+++ src/arrayofobject/printdetails2.java
@@ -2,7 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */
+ this is some pull practice  sadas d ad asdas*/
 package arrayofobject;
 
 import java.util.Scanner;
